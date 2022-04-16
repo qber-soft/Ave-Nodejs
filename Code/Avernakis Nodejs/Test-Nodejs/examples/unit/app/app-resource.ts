b8dd9c9df1a0529d3e6c5001cc1794f08292c7e0
rerender2021 <rerender2021@outlook.com>
2022-04-16 18:00:31 +0800

    impl createResourceMap

--- Code/Avernakis Nodejs/Test-Nodejs/examples/unit/app/app-resource.ts
+++ Code/Avernakis Nodejs/Test-Nodejs/examples/unit/app/app-resource.ts
@@ -5,15 +5,12 @@ import * as path from "path";
 
 export function run() {
 	const app = new App();
-	app.ResSetIconSizeList([16]);
 
-	const openFileIcon = fs.readFileSync(path.resolve(__dirname, "./FileOpen#0.png"));
-	app.ResAddResourceProvider((id) => {
-		switch (id) {
-			case 0x10:
-				return ResourceSource.ToArrayBuffer(openFileIcon);
-		}
-	});
+	app.ResSetIconSizeList([16]);
+	const iconDataMap = {
+		Open: [fs.readFileSync(path.resolve(__dirname, "./FileOpen#0.png"))],
+	};
+	const resMap = createResourceMap(app, iconDataMap);
 
 	globalThis.app = app;
 
@@ -28,8 +25,7 @@ export function run() {
 		const button = new Button(window);
 		button.SetText("Open");
 		button.SetVisualTextLayout(VisualTextLayout.HorzVisualText);
-		const id = 0x10;
-		const iconSource = new IconSource(id, 16);
+		const iconSource = new IconSource(resMap.get("Open"), 16);
 		const icon = window.CreateManagedIcon(iconSource);
 		button.SetVisual(icon);
 
@@ -43,4 +39,40 @@ export function run() {
 
 	window.SetVisible(true);
 	window.Activate();
+}
+
+class ResourceMap<Name extends string = string> {
+	private map: Record<Name, number>;
+
+	constructor(map: Record<Name, number>) {
+		this.map = map;
+	}
+
+	get(name: Name): number {
+		return this.map[name];
+	}
+}
+
+function createResourceMap<IconDataMap extends Record<string, Buffer[]>, Name extends string = keyof IconDataMap & string>(app: App, iconDataMap: IconDataMap): ResourceMap<Name> {
+	const map: Record<string, number> = {};
+	const provider: Record<number, Buffer> = {};
+
+	const baseId = 0x10;
+	const deltaId = 0x10;
+
+	Object.keys(iconDataMap).forEach((name, iconIndex) => {
+		const dataList = iconDataMap[name];
+		dataList.forEach((buffer, dataIndex) => {
+			const id = baseId + iconIndex * deltaId + dataIndex;
+			if (dataIndex === 0) {
+				map[name] = id;
+			}
+			provider[id] = buffer;
+		});
+	});
+
+	app.ResAddResourceProvider((id) => ResourceSource.ToArrayBuffer(provider[id]));
+
+	const resMap = new ResourceMap(map);
+	return resMap;
 }
