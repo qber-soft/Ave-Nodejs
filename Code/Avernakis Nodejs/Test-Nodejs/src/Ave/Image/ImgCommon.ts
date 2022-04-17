45c87eb6c7baf8812aad13b89d1225730ab89488
ivjtk <ivjtk@ivjtk.ivjtk>
2022-04-17 23:55:04 +0800

    Add ImageData.SetPixel. Fix some bugs in ImageData.GetPixel.

--- Code/Avernakis Nodejs/Test-Nodejs/src/Ave/Image/ImgCommon.ts
+++ Code/Avernakis Nodejs/Test-Nodejs/src/Ave/Image/ImgCommon.ts
@@ -52,16 +52,21 @@ export class ImageMetadata {
 	}
 
 	ComputeSubresourceCount() {
-		if (ImageDimension.Texture3D != this.Dimension) return this.DepthOrArraySize * this.MipLevel;
-		else return this.MipLevel;
+		if (ImageDimension.Texture3D != this.Dimension)
+			return this.DepthOrArraySize * this.MipLevel;
+		else
+			return this.MipLevel;
 	}
 
 	ComputeSubresourceIndex(mipIndex: number, arrayIndex: number) {
-		if (mipIndex >= this.MipLevel) return -1;
+		if (mipIndex >= this.MipLevel)
+			return -1;
 		if (ImageDimension.Texture3D != this.Dimension) {
-			if (arrayIndex >= this.DepthOrArraySize) return -1;
+			if (arrayIndex >= this.DepthOrArraySize)
+				return -1;
 		} else {
-			if (0 != arrayIndex) return -1;
+			if (0 != arrayIndex)
+				return -1;
 		}
 		return arrayIndex * this.MipLevel + mipIndex;
 	}
@@ -95,7 +100,7 @@ export enum BlendType {
 	Src_Alpha_Sat    /**/ = 11,
 	Blend_Factor     /**/ = 14,
 	Inv_Blend_Factor /**/ = 15,
-}
+};
 
 // prettier-ignore
 export enum BlendOp {
@@ -104,7 +109,7 @@ export enum BlendOp {
 	Rev_Subtract /**/ = 3,
 	Min          /**/ = 4,
 	Max          /**/ = 5,
-}
+};
 
 export enum ColorWriteFlag {
 	None = 0,
@@ -174,6 +179,7 @@ export class ImageData {
 	View: DataView;
 
 	private m_Get: (x, y, z) => Vec4;
+	private m_Set: (x, y, z, v) => void;
 
 	static FromNative(d: ImageData) {
 		const data = new ImageData();
@@ -187,11 +193,16 @@ export class ImageData {
 		data.View = new DataView(data.Data);
 
 		data.m_Get = data[`G_${PixFormat[data.Format]}`] ?? data.G_Empty;
+		data.m_Set = data[`S_${PixFormat[data.Format]}`] ?? data.S_Empty;
 		return data;
 	}
 
 	GetPixel(x: number = 0, y: number = 0, z: number = 0): Vec4 {
 		return this.m_Get(x, y, z);
+	}
+
+	SetPixel(x: number = 0, y: number = 0, z: number = 0, v: Vec4) {
+		return this.m_Set(x, y, z, v);
 	}
 
 	private G_R32_4(x, y, z) { const o = z * this.SlicePitch + y * this.RowPitch + x * 16; return new Vec4(this.View.getFloat32(o), this.View.getFloat32(o + 4), this.View.getFloat32(o + 8), this.View.getFloat32(o + 12)); }
@@ -211,7 +222,7 @@ export class ImageData {
 	private G_U16_2(x, y, z) { const o = z * this.SlicePitch + y * this.RowPitch + x * 4; return new Vec4(this.View.getUint16(o), this.View.getUint16(o + 2), 0, 0); }
 	private G_S16_2(x, y, z) { const o = z * this.SlicePitch + y * this.RowPitch + x * 4; return new Vec4(this.View.getInt16(o), this.View.getInt16(o + 2), 0, 0); }
 
-	private G_U16_3_U2(x, y, z) { const d = this.View.getUint32(z * this.SlicePitch + y * this.RowPitch + x * 4); return new Vec4(0x3ff & d, 0x3ff & (d >> 10), 0x3ff & (d >> 20), 0x3 & (d >> 30)); }
+	private G_U10_3_U2(x, y, z) { const d = this.View.getUint32(z * this.SlicePitch + y * this.RowPitch + x * 4); return new Vec4(0x3ff & d, 0x3ff & (d >> 10), 0x3ff & (d >> 20), 0x3 & (d >> 30)); }
 
 	private G_U8_4(x, y, z) { const o = z * this.SlicePitch + y * this.RowPitch + x * 4; return new Vec4(this.View.getUint8(o), this.View.getUint8(o + 1), this.View.getUint8(o + 2), this.View.getUint8(o + 3)); }
 	private G_S8_4(x, y, z) { const o = z * this.SlicePitch + y * this.RowPitch + x * 4; return new Vec4(this.View.getInt8(o), this.View.getInt8(o + 1), this.View.getInt8(o + 2), this.View.getInt8(o + 3)); }
@@ -221,6 +232,36 @@ export class ImageData {
 	private G_U4_4(x, y, z) { const d = this.View.getUint16(z * this.SlicePitch + y * this.RowPitch + x * 2); return new Vec4(0xf & d, 0xf & (d >> 4), 0xf & (d >> 8), 0xf & (d >> 12)); }
 
 	private G_Empty(x, y, z) { return Vec4.Zero; }
+	
+
+	private S_R32_4(x, y, z, v) { const o = z * this.SlicePitch + y * this.RowPitch + x * 16; this.View.setFloat32(o, v.r); this.View.setFloat32(o + 4, v.g); this.View.setFloat32(o + 8, v.b); this.View.setFloat32(o + 12, v.a); }
+	private S_U32_4(x, y, z, v) { const o = z * this.SlicePitch + y * this.RowPitch + x * 16; this.View.setUint32(o, v.r); this.View.setUint32(o + 4, v.g); this.View.setUint32(o + 8, v.b); this.View.setUint32(o + 12, v.a); }
+	private S_S32_4(x, y, z, v) { const o = z * this.SlicePitch + y * this.RowPitch + x * 16; this.View.setInt32(o, v.r); this.View.setInt32(o + 4, v.g); this.View.setInt32(o + 8, v.b); this.View.setInt32(o + 12, v.a); }
+	private S_R32_3(x, y, z, v) { const o = z * this.SlicePitch + y * this.RowPitch + x * 12; this.View.setFloat32(o, v.r); this.View.setFloat32(o + 4, v.g); this.View.setFloat32(o + 8, v.b); }
+	private S_U32_3(x, y, z, v) { const o = z * this.SlicePitch + y * this.RowPitch + x * 12; this.View.setUint32(o, v.r); this.View.setUint32(o + 4, v.g); this.View.setUint32(o + 8, v.b); }
+	private S_S32_3(x, y, z, v) { const o = z * this.SlicePitch + y * this.RowPitch + x * 12; this.View.setInt32(o, v.r); this.View.setInt32(o + 4, v.g); this.View.setInt32(o + 8, v.b); }
+	private S_R32_2(x, y, z, v) { const o = z * this.SlicePitch + y * this.RowPitch + x * 8; this.View.setFloat32(o, v.r); this.View.setFloat32(o + 4, v.g); }
+	private S_U32_2(x, y, z, v) { const o = z * this.SlicePitch + y * this.RowPitch + x * 8; this.View.setUint32(o, v.r); this.View.setUint32(o + 4, v.g); }
+	private S_S32_2(x, y, z, v) { const o = z * this.SlicePitch + y * this.RowPitch + x * 8; this.View.setInt32(o, v.r); this.View.setInt32(o + 4, v.g); }
+
+	private S_R16_4(x, y, z, v) { const o = z * this.SlicePitch + y * this.RowPitch + x * 8; AveMath.WriteR16(this.View, o, v.r); AveMath.WriteR16(this.View, o + 2, v.g); AveMath.WriteR16(this.View, o + 4, v.z); AveMath.WriteR16(this.View, o + 6, v.a); }
+	private S_U16_4(x, y, z, v) { const o = z * this.SlicePitch + y * this.RowPitch + x * 8; this.View.setUint16(o, v.r); this.View.setUint16(o + 2, v.g); this.View.setUint16(o + 4, v.b); this.View.setUint16(o + 6, v.a); }
+	private S_S16_4(x, y, z, v) { const o = z * this.SlicePitch + y * this.RowPitch + x * 8; this.View.setInt16(o, v.r); this.View.setInt16(o + 2, v.g); this.View.setInt16(o + 4, v.b); this.View.setInt16(o + 6, v.a); }
+	private S_R16_2(x, y, z, v) { const o = z * this.SlicePitch + y * this.RowPitch + x * 4; AveMath.WriteR16(this.View, o, v.r); AveMath.WriteR16(this.View, o + 2, v.g); }
+	private S_U16_2(x, y, z, v) { const o = z * this.SlicePitch + y * this.RowPitch + x * 4; this.View.setUint16(o, v.r); this.View.setUint16(o + 2, v.g); }
+	private S_S16_2(x, y, z, v) { const o = z * this.SlicePitch + y * this.RowPitch + x * 4; this.View.setInt16(o, v.r); this.View.setInt16(o + 2, v.g); }
+
+	private S_U10_3_U2(x, y, z, v) { const d = (0x3ff & v.r) | (0x3ff & v.g) << 10 | (0x3ff & v.b) << 20 | (0x3 & v.a) << 30; this.View.setUint32(z * this.SlicePitch + y * this.RowPitch + x * 4, d); }
+
+	private S_U8_4(x, y, z, v) { const o = z * this.SlicePitch + y * this.RowPitch + x * 4; this.View.setUint8(o, v.r); this.View.setUint8(o + 4, v.g); this.View.setUint8(o + 8, v.b); this.View.setUint8(o + 12, v.a); }
+	private S_S8_4(x, y, z, v) { const o = z * this.SlicePitch + y * this.RowPitch + x * 4; this.View.setInt8(o, v.r); this.View.setInt8(o + 4, v.g); this.View.setInt8(o + 8, v.b); this.View.setInt8(o + 12, v.a); }
+	private S_U8_2(x, y, z, v) { const o = z * this.SlicePitch + y * this.RowPitch + x * 2; this.View.setUint8(o, v.r); this.View.setUint8(o + 4, v.g); }
+	private S_S8_2(x, y, z, v) { const o = z * this.SlicePitch + y * this.RowPitch + x * 2; this.View.setInt8(o, v.r); this.View.setInt8(o + 4, v.g); }
+
+	private S_U4_4(x, y, z, v) { const d = (0xf & v.r) | (0xf & v.g) << 4 | (0xf & v.b) << 8 | (0xf & v.a) << 12; this.View.setUint16(z * this.SlicePitch + y * this.RowPitch + x * 2, d); }
+
+	private S_Empty(x, y, z, v) {}
+
 
 	private G_R32G32B32A32_FLOAT          /**/(x, y, z) { return this.G_R32_4(x, y, z); }
 	private G_R32G32B32A32_UINT           /**/(x, y, z) { return this.G_U32_4(x, y, z); }
@@ -239,8 +280,8 @@ export class ImageData {
 	private G_D32_FLOAT_S8X24_UINT        /**/(x, y, z) { const o = z * this.SlicePitch + y * this.RowPitch + x * 8; return new Vec4(this.View.getFloat32(o), this.View.getUint8(o + 4), 0, 0); }
 	private G_R32_FLOAT_X8X24_TYPELESS    /**/(x, y, z) { const o = z * this.SlicePitch + y * this.RowPitch + x * 8; return new Vec4(this.View.getFloat32(o), 0, 0, 0); }
 	private G_X32_TYPELESS_G8X24_UINT     /**/(x, y, z) { const o = z * this.SlicePitch + y * this.RowPitch + x * 8; return new Vec4(0, this.View.getUint8(o + 4), 0, 0); }
-	private G_R10G10B10A2_UNORM           /**/(x, y, z) { return this.G_U16_3_U2(x, y, z).Div(new Vec4(1023, 1023, 1023, 3)); }
-	private G_R10G10B10A2_UINT            /**/(x, y, z) { return this.G_U16_3_U2(x, y, z); }
+	private G_R10G10B10A2_UNORM           /**/(x, y, z) { return this.G_U10_3_U2(x, y, z).Div(new Vec4(1023, 1023, 1023, 3)); }
+	private G_R10G10B10A2_UINT            /**/(x, y, z) { return this.G_U10_3_U2(x, y, z); }
 	//private G_R11G11B10_FLOAT             /**/(x, y, z) { return Vec4.Zero; } // TODO: Implement R11 and R10
 	private G_R8G8B8A8_UNORM              /**/(x, y, z) { return this.G_U8_4(x, y, z).Div(255); }
 	private G_R8G8B8A8_UNORM_SRGB         /**/(x, y, z) { return this.G_U8_4(x, y, z).Div(255); }
@@ -268,7 +309,7 @@ export class ImageData {
 	private G_R16_UNORM                   /**/(x, y, z) { return new Vec4(this.View.getUint16(z * this.SlicePitch + y * this.RowPitch + x * 2) / 65535, 0, 0, 0); }
 	private G_R16_UINT                    /**/(x, y, z) { return new Vec4(this.View.getUint16(z * this.SlicePitch + y * this.RowPitch + x * 2), 0, 0, 0); }
 	private G_R16_SNORM                   /**/(x, y, z) { return new Vec4(this.View.getInt16(z * this.SlicePitch + y * this.RowPitch + x * 2) / 32768, 0, 0, 0); }
-	private G_R16_SINT                    /**/(x, y, z) { return new Vec4(this.View.getInt16(z * this.SlicePitch + y * this.RowPitch + x * 2) / 32768, 0, 0, 0); }
+	private G_R16_SINT                    /**/(x, y, z) { return new Vec4(this.View.getInt16(z * this.SlicePitch + y * this.RowPitch + x * 2), 0, 0, 0); }
 	private G_R8_UNORM                    /**/(x, y, z) { return new Vec4(this.View.getUint8(z * this.SlicePitch + y * this.RowPitch + x) / 255, 0, 0, 0); }
 	private G_R8_UINT                     /**/(x, y, z) { return new Vec4(this.View.getUint8(z * this.SlicePitch + y * this.RowPitch + x), 0, 0, 0); }
 	private G_R8_SNORM                    /**/(x, y, z) { return new Vec4(this.View.getInt8(z * this.SlicePitch + y * this.RowPitch + x) / 128, 0, 0, 0); }
@@ -308,6 +349,74 @@ export class ImageData {
 	private G_P8                          /**/(x, y, z) { return new Vec4(this.View.getUint8(z * this.SlicePitch + y * this.RowPitch + x) / 255, 0, 0, 0); }
 	private G_A8P8                        /**/(x, y, z) { return this.G_U8_2(x, y, z).Div(255); }
 	private G_B4G4R4A4_UNORM              /**/(x, y, z) { const d = this.G_U4_4(x, y, z).Div(15); return new Vec4(d.b, d.g, d.r, d.a); }
+
+
+	private S_R32G32B32A32_FLOAT          /**/(x, y, z, v) { this.S_R32_4(x, y, z, v); }
+	private S_R32G32B32A32_UINT           /**/(x, y, z, v) { this.S_U32_4(x, y, z, v); }
+	private S_R32G32B32A32_SINT           /**/(x, y, z, v) { this.S_S32_4(x, y, z, v); }
+	private S_R32G32B32_FLOAT             /**/(x, y, z, v) { this.S_R32_3(x, y, z, v); }
+	private S_R32G32B32_UINT              /**/(x, y, z, v) { this.S_U32_3(x, y, z, v); }
+	private S_R32G32B32_SINT              /**/(x, y, z, v) { this.S_S32_3(x, y, z, v); }
+	private S_R16G16B16A16_FLOAT          /**/(x, y, z, v) { this.S_R16_4(x, y, z, v); }
+	private S_R16G16B16A16_UNORM          /**/(x, y, z, v) { this.S_U16_4(x, y, z, v.Mul(65535)); }
+	private S_R16G16B16A16_UINT           /**/(x, y, z, v) { this.S_U16_4(x, y, z, v); }
+	private S_R16G16B16A16_SNORM          /**/(x, y, z, v) { this.S_S16_4(x, y, z, v.Mul(32768)); }
+	private S_R16G16B16A16_SINT           /**/(x, y, z, v) { this.S_S16_4(x, y, z, v); }
+	private S_R32G32_FLOAT                /**/(x, y, z, v) { this.S_R32_2(x, y, z, v); }
+	private S_R32G32_UINT                 /**/(x, y, z, v) { this.S_U32_2(x, y, z, v); }
+	private S_R32G32_SINT                 /**/(x, y, z, v) { this.S_S32_2(x, y, z, v); }
+	private S_D32_FLOAT_S8X24_UINT        /**/(x, y, z, v) { const o = z * this.SlicePitch + y * this.RowPitch + x * 8; this.View.setFloat32(o, v.r); this.View.setUint8(o + 4, v.g); }
+	private S_R32_FLOAT_X8X24_TYPELESS    /**/(x, y, z, v) { const o = z * this.SlicePitch + y * this.RowPitch + x * 8; this.View.setFloat32(o, v.r); }
+	private S_X32_TYPELESS_G8X24_UINT     /**/(x, y, z, v) { const o = z * this.SlicePitch + y * this.RowPitch + x * 8; this.View.setUint8(o + 4, v.g); }
+	private S_R10G10B10A2_UNORM           /**/(x, y, z, v) { this.S_U10_3_U2(x, y, z, v.Mul(new Vec4(1023, 1023, 1023, 3))); }
+	private S_R10G10B10A2_UINT            /**/(x, y, z, v) { this.S_U10_3_U2(x, y, z, v); }
+	//private S_R11G11B10_FLOAT             /**/(x, y, z, v) {} // TODO: Implement R11 and R10
+	private S_R8G8B8A8_UNORM              /**/(x, y, z, v) { this.S_U8_4(x, y, z, v.Mul(255)); }
+	private S_R8G8B8A8_UNORM_SRGB         /**/(x, y, z, v) { this.S_U8_4(x, y, z, v.Mul(255)); }
+	private S_R8G8B8A8_UINT               /**/(x, y, z, v) { this.S_U8_4(x, y, z, v); }
+	private S_R8G8B8A8_SNORM              /**/(x, y, z, v) { this.S_S8_4(x, y, z, v.Mul(128)); }
+	private S_R8G8B8A8_SINT               /**/(x, y, z, v) { this.S_S8_4(x, y, z, v); }
+	private S_R16G16_FLOAT                /**/(x, y, z, v) { this.S_R16_2(x, y, z, v); }
+	private S_R16G16_UNORM                /**/(x, y, z, v) { this.S_U16_2(x, y, z, v.Mul(65535)); }
+	private S_R16G16_UINT                 /**/(x, y, z, v) { this.S_U16_2(x, y, z, v); }
+	private S_R16G16_SNORM                /**/(x, y, z, v) { this.S_S16_4(x, y, z, v.Mul(32768)); }
+	private S_R16G16_SINT                 /**/(x, y, z, v) { this.S_S16_4(x, y, z, v); }
+	private S_D32_FLOAT                   /**/(x, y, z, v) { this.View.setFloat32(z * this.SlicePitch + y * this.RowPitch + x * 4, v.r); }
+	private S_R32_FLOAT                   /**/(x, y, z, v) { this.View.setFloat32(z * this.SlicePitch + y * this.RowPitch + x * 4, v.r); }
+	private S_R32_UINT                    /**/(x, y, z, v) { this.View.setUint32(z * this.SlicePitch + y * this.RowPitch + x * 4, v.r); }
+	private S_R32_SINT                    /**/(x, y, z, v) { this.View.setInt32(z * this.SlicePitch + y * this.RowPitch + x * 4, v.r); }
+	private S_D24_UNORM_S8_UINT           /**/(x, y, z, v) { const d = (0xffffff & (16777215 * v.r)) | (0xff & v.g) << 24; this.View.setUint32(z * this.SlicePitch + y * this.RowPitch + x * 4, d); }
+	private S_R24_UNORM_X8_TYPELESS       /**/(x, y, z, v) { const d = 0xffffff & (16777215 * v.r); this.View.setUint32(z * this.SlicePitch + y * this.RowPitch + x * 4, d); }
+	private S_X24_TYPELESS_G8_UINT        /**/(x, y, z, v) { const d = (0xff & v.g) << 24; this.View.setUint32(z * this.SlicePitch + y * this.RowPitch + x * 4, d); }
+	private S_R8G8_UNORM                  /**/(x, y, z, v) { this.S_U8_2(x, y, z, v.Mul(255)); }
+	private S_R8G8_UINT                   /**/(x, y, z, v) { this.S_U8_2(x, y, z, v); }
+	private S_R8G8_SNORM                  /**/(x, y, z, v) { this.S_S8_2(x, y, z, v.Mul(255)); }
+	private S_R8G8_SINT                   /**/(x, y, z, v) { this.S_S8_2(x, y, z, v); }
+	private S_R16_FLOAT                   /**/(x, y, z, v) { AveMath.WriteR16(this.View, z * this.SlicePitch + y * this.RowPitch + x * 2, v.r); }
+	private S_D16_UNORM                   /**/(x, y, z, v) { this.View.setUint16(z * this.SlicePitch + y * this.RowPitch + x * 2, v.r * 65535); }
+	private S_R16_UNORM                   /**/(x, y, z, v) { this.View.setUint16(z * this.SlicePitch + y * this.RowPitch + x * 2, v.r * 65535); }
+	private S_R16_UINT                    /**/(x, y, z, v) { this.View.setUint16(z * this.SlicePitch + y * this.RowPitch + x * 2, v.r); }
+	private S_R16_SNORM                   /**/(x, y, z, v) { this.View.setInt16(z * this.SlicePitch + y * this.RowPitch + x * 2, v.r * 32768); }
+	private S_R16_SINT                    /**/(x, y, z, v) { this.View.setInt16(z * this.SlicePitch + y * this.RowPitch + x * 2, v.r); }
+	private S_R8_UNORM                    /**/(x, y, z, v) { this.View.setUint8(z * this.SlicePitch + y * this.RowPitch + x, v.r * 255); }
+	private S_R8_UINT                     /**/(x, y, z, v) { this.View.setUint8(z * this.SlicePitch + y * this.RowPitch + x, v.r); }
+	private S_R8_SNORM                    /**/(x, y, z, v) { this.View.setInt8(z * this.SlicePitch + y * this.RowPitch + x, v.r * 128); }
+	private S_R8_SINT                     /**/(x, y, z, v) { this.View.setInt8(z * this.SlicePitch + y * this.RowPitch + x, v.r); }
+	private S_A8_UNORM                    /**/(x, y, z, v) { this.View.setUint8(z * this.SlicePitch + y * this.RowPitch + x, v.r * 255); }
+	private S_R1_UNORM                    /**/(x, y, z, v) { const o = z * this.SlicePitch + y * this.RowPitch + (x >> 3); this.View.setUint8(o, this.View.getUint8(o) & ~(1 << (7 & x)) | (v.r ? 1 : 0) << (7 & x)); }
+	//private S_R9G9B9E5_SHAREDEXP          /**/(x, y, z, v) {} // TODO: Implement R9G9B9E5_SHAREDEXP
+
+	private S_B5G6R5_UNORM                /**/(x, y, z, v) { const d = (0x1f & (v.b * 31)) | (0x3f & (v.g * 63)) << 5 | (0x1f & (v.r * 31)) << 11; this.View.setUint32(z * this.SlicePitch + y * this.RowPitch + x * 2, d); }
+	private S_B5G5R5A1_UNORM              /**/(x, y, z, v) { const d = (0x1f & (v.b * 31)) | (0x1f & (v.g * 31)) << 5 | (0x1f & (v.r * 31)) << 10 | (0x1 & v.r) << 15; this.View.setUint32(z * this.SlicePitch + y * this.RowPitch + x * 2, d); }
+	private S_B8G8R8A8_UNORM              /**/(x, y, z, v) { const d = new Vec4(v.b, v.g, v.r, v.a); this.S_U8_4(x, y, z, v.Mul(255)); }
+	private S_B8G8R8X8_UNORM              /**/(x, y, z, v) { const d = new Vec4(v.b, v.g, v.r, 1); this.S_U8_4(x, y, z, v.Mul(255)); }
+	// private S_R10G10B10_XR_BIAS_A2_UNORM  /**/(x, y, z, v) {} // TODO: Implement R10G10B10_XR_BIAS_A2_UNORM
+	private S_B8G8R8A8_UNORM_SRGB         /**/(x, y, z, v) { const d = new Vec4(v.b, v.g, v.r, v.a); this.S_U8_4(x, y, z, v.Mul(255)); }
+	private S_B8G8R8X8_UNORM_SRGB         /**/(x, y, z, v) { const d = new Vec4(v.b, v.g, v.r, 1); this.S_U8_4(x, y, z, v.Mul(255)); }
+
+	private S_P8                          /**/(x, y, z, v) { this.View.setUint8(z * this.SlicePitch + y * this.RowPitch + x, v.r * 255); }
+	private S_A8P8                        /**/(x, y, z, v) { this.S_U8_2(x, y, z, v.Mul(255)); }
+	private S_B4G4R4A4_UNORM              /**/(x, y, z, v) { const d = new Vec4(v.b, v.g, v.r, v.a); this.S_U4_4(x, y, z, v.Mul(15)); }
 }
 
 // prettier-ignore
--- Code/Avernakis Nodejs/Test-Nodejs/src/Ave/Math/Math.ts
+++ Code/Avernakis Nodejs/Test-Nodejs/src/Ave/Math/Math.ts
@@ -5,22 +5,18 @@
 		const f = dv.getUint16(nByteOffset);
 		let Mantissa = 0x3ff & f;
 		let Exponent;
-		if ( (f & 0x7C00) != 0 )
-		{
+		if ((f & 0x7C00) != 0) {
 			Exponent = (f >> 10) & 0x1f;
 		}
-		else if ( Mantissa != 0 )
-		{
+		else if (Mantissa != 0) {
 			Exponent = 1;
-			do
-			{
+			do {
 				Exponent--;
 				Mantissa <<= 1;
-			} while ( (Mantissa & 0x0400) == 0 );
+			} while ((Mantissa & 0x0400) == 0);
 			Mantissa &= 0x03ff;
 		}
-		else
-		{
+		else {
 			Exponent = -112;
 		}
 		const Result = ((f & 0x8000) << 16) | ((Exponent + 112) << 23) | (Mantissa << 13);
@@ -29,5 +25,8 @@
 		data.setUint32(0, Result);
 		return data.getFloat32(0);
 	}
-	
+
+	static WriteR16(dv: DataView, nByteOffset: number, f: number) {
+
+	}
 }
