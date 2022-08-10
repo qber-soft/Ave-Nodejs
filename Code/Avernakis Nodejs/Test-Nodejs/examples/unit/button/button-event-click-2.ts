689c02ab584a0b7f889e0dc4bd35efab01e79bda
rerender2021 <rerender2021@outlook.com>
2022-08-10 22:50:46 +0800

    update lib; add comment for example

--- Code/Avernakis Nodejs/Test-Nodejs/examples/unit/button/button-event-click-2.ts
+++ Code/Avernakis Nodejs/Test-Nodejs/examples/unit/button/button-event-click-2.ts
@@ -1,6 +1,7 @@
 import { Window, Button } from "../../../src";
 import { getControlDemoContainer } from "../utility";
 
+// expect: only print "on click 2" once
 export function main(window: Window) {
 	const button = new Button(window);
 	button.SetText("Button");
