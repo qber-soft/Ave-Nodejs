891118e03d255d7dec24d90a0afb72116923dcff
rerender2021 <rerender2021@outlook.com>
2022-11-06 12:57:53 +0800

    fix test case

--- Code/Avernakis Nodejs/Test-Nodejs/test/time-point.test.ts
+++ Code/Avernakis Nodejs/Test-Nodejs/test/time-point.test.ts
@@ -2,12 +2,11 @@ import { TimePoint } from "../src";
 
 describe("TimePoint", () => {
 	test("from js date time", () => {
-		const now = 1667709843452;
+		const now = 1667709843452; // Sun Nov 06 2022 12:44:03 GMT+0800 (China Standard Time)
 		const time = TimePoint.FromJsDateTime(now);
 
-		const date = new Date(now); // Sun Nov 06 2022 12:44:03 GMT+0800 (China Standard Time)
-		expect(time.Year).toEqual(date.getFullYear());
-		expect(time.Month).toEqual(date.getMonth());
-		expect(time.Day).toEqual(date.getDay());
+		expect(time.Year).toEqual(2022);
+		expect(time.Month).toEqual(11);
+		expect(time.Day).toEqual(6);
 	});
 });
