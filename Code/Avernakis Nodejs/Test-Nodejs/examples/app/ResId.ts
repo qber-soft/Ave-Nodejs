d097115aa8aff1e3da091264eb663f08cd564340
rerender2021 <rerender2021@outlook.com>
2022-11-08 21:33:26 +0800

    update res and compiler

--- Code/Avernakis Nodejs/Test-Nodejs/examples/app/ResId.ts
+++ Code/Avernakis Nodejs/Test-Nodejs/examples/app/ResId.ts
@@ -1,23 +1,24 @@
 ﻿export enum ResId {
-	// 126 files in 'Icon'
-	Icon_Clock_png = 0x00000010, // Icon\Clock#0.png
-	Icon_Copy_png = 0x00000020, // Icon\Copy#0.png
-	Icon_Cut_png = 0x00000030, // Icon\Cut#0.png
-	Icon_Delete_png = 0x00000040, // Icon\Delete#0.png
-	Icon_Exit_png = 0x00000050, // Icon\Exit#0.png
-	Icon_File_png = 0x00000060, // Icon\File#0.png
-	Icon_FileNew_png = 0x00000070, // Icon\FileNew#0.png
-	Icon_FileOpen_png = 0x00000080, // Icon\FileOpen#0.png
-	Icon_FileSave_png = 0x00000090, // Icon\FileSave#0.png
-	Icon_FileSaveAs_png = 0x000000a0, // Icon\FileSaveAs#0.png
-	Icon_Folder_png = 0x000000b0, // Icon\Folder#0.png
-	Icon_Globe_png = 0x000000c0, // Icon\Globe#0.png
-	Icon_Language_png = 0x000000d0, // Icon\Language#0.png
-	Icon_Paste_png = 0x000000e0, // Icon\Paste#0.png
-	Icon_Redo_png = 0x000000f0, // Icon\Redo#0.png
-	Icon_Ui_png = 0x00000100, // Icon\Ui#0.png
-	Icon_Undo_png = 0x00000110, // Icon\Undo#0.png
-	Icon_ZoomIn_png = 0x00000120, // Icon\ZoomIn#0.png
+	// 133 files in 'Icon'
+	Icon_Ave_png = 0x00000010, // Icon\Ave#0.png
+	Icon_Clock_png = 0x00000020, // Icon\Clock#0.png
+	Icon_Copy_png = 0x00000030, // Icon\Copy#0.png
+	Icon_Cut_png = 0x00000040, // Icon\Cut#0.png
+	Icon_Delete_png = 0x00000050, // Icon\Delete#0.png
+	Icon_Exit_png = 0x00000060, // Icon\Exit#0.png
+	Icon_File_png = 0x00000070, // Icon\File#0.png
+	Icon_FileNew_png = 0x00000080, // Icon\FileNew#0.png
+	Icon_FileOpen_png = 0x00000090, // Icon\FileOpen#0.png
+	Icon_FileSave_png = 0x000000a0, // Icon\FileSave#0.png
+	Icon_FileSaveAs_png = 0x000000b0, // Icon\FileSaveAs#0.png
+	Icon_Folder_png = 0x000000c0, // Icon\Folder#0.png
+	Icon_Globe_png = 0x000000d0, // Icon\Globe#0.png
+	Icon_Language_png = 0x000000e0, // Icon\Language#0.png
+	Icon_Paste_png = 0x000000f0, // Icon\Paste#0.png
+	Icon_Redo_png = 0x00000100, // Icon\Redo#0.png
+	Icon_Ui_png = 0x00000110, // Icon\Ui#0.png
+	Icon_Undo_png = 0x00000120, // Icon\Undo#0.png
+	Icon_ZoomIn_png = 0x00000130, // Icon\ZoomIn#0.png
 	$AppRes_Icon_Min = 0x00000010,
-	$AppRes_Icon_Max = 0x00000126,
+	$AppRes_Icon_Max = 0x00000136,
 }
