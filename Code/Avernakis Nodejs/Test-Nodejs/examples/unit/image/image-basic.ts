c9cb58aefa717b4ab2136db8c965637174bf9857
rerender2021 <rerender2021@outlook.com>
2022-11-18 00:31:07 +0800

    refine image basic exmaple

--- Code/Avernakis Nodejs/Test-Nodejs/examples/unit/image/image-basic.ts
+++ Code/Avernakis Nodejs/Test-Nodejs/examples/unit/image/image-basic.ts
@@ -10,11 +10,14 @@ export function main(window: Window, { app }: { app: App }) {
 	const source = ResourceSource.FromBuffer(buffer);
 	const aveImage = codec.Open(source);
 
+	const picture = new Picture(window);
 	{
 		const arrayBuffer = codec.SaveArrayBuffer(aveImage, ImageContainerType.PNG);
-		console.log(arrayBuffer);
+		const source = ResourceSource.FromBuffer(Buffer.from(arrayBuffer));
+		picture.SetPicture(source);
 	}
 
 	const container = getControlDemoContainer(window, 1, 300, 300);
+	container.ControlAdd(picture).SetGrid(1, 1);
 	window.SetContent(container);
 }
